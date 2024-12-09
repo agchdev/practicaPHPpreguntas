a01ead0f4220b6ea4371f75d666b3938cbd7a79b
unknown <alex.chinchi02@gmail.com>
2024-12-09 21:36:11 +0100

    cambios

--- practica2.sql
+++ practica2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2024 a las 13:02:14
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 09-12-2024 a las 21:32:12
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,9 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `preguntas` (
   `idPregunta` int(11) NOT NULL,
-  `textPregunta` int(11) NOT NULL,
+  `textPregunta` varchar(300) NOT NULL,
   `respuestaPregunta` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `preguntas`
+--
+
+INSERT INTO `preguntas` (`idPregunta`, `textPregunta`, `respuestaPregunta`) VALUES
+(1, '¿Qué tipo de animal es la ballena?', 'mamífero'),
+(2, '¿Quién pintó “la última cena”?', 'Leonardo da Vinci'),
+(3, '¿En qué país se encuentra la torre de Pisa?', 'Italia'),
+(4, '¿Cuál es el cuadrado ? 64?	', '8'),
+(5, '¿Cuántos segundos hay en un día?', '86400'),
+(6, '¿Valor de cos 360°?', '1');
 
 -- --------------------------------------------------------
 
@@ -52,7 +64,11 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`usuario`, `tmpInicio`, `tmpFinal`, `tmpTotal`) VALUES
 ('', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('alejandro', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+('alejandro', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('cacatua', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('perro', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('perro2', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('usuarioNuevo', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
 -- Índices para tablas volcadas
@@ -78,7 +94,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `preguntas`
 --
 ALTER TABLE `preguntas`
-  MODIFY `idPregunta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
