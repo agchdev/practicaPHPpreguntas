8481824524ecd5dd5417962040a52a7ae2d64aa2
unknown <alex.chinchi02@gmail.com>
2024-12-10 14:21:20 +0100

    cambios

--- practica2.sql
+++ practica2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-12-2024 a las 21:32:12
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 10-12-2024 a las 14:19:38
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,11 @@ INSERT INTO `preguntas` (`idPregunta`, `textPregunta`, `respuestaPregunta`) VALU
 (3, '¿En qué país se encuentra la torre de Pisa?', 'Italia'),
 (4, '¿Cuál es el cuadrado ? 64?	', '8'),
 (5, '¿Cuántos segundos hay en un día?', '86400'),
-(6, '¿Valor de cos 360°?', '1');
+(6, '¿Valor de cos 360°?', '1'),
+(7, '¿En qué país y año se usó la primera bomba atómica?', 'Japón,1945'),
+(8, '¿Dónde y cuando ganó España el mundial?', 'Sudáfrica,2010'),
+(9, '¿Quienes son los favoritos de Erica?', 'Alejandro Aguayo,Juanca'),
+(10, 'Nombra los colores primarios:', 'rojo,amarillo,azul');
 
 -- --------------------------------------------------------
 
@@ -63,12 +67,16 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`usuario`, `tmpInicio`, `tmpFinal`, `tmpTotal`) VALUES
-('', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 ('alejandro', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('cacatua', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('perro', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('perro2', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('usuarioNuevo', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+('caracaqui', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('ddd', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('dwdw', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('dwdwdwd', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('mecagoentodoya2', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('PerroLoco', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('PerroLoco222', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('PerroLoco4', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('PerroLoco5', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
 -- Índices para tablas volcadas
@@ -94,7 +102,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `preguntas`
 --
 ALTER TABLE `preguntas`
-  MODIFY `idPregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idPregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
